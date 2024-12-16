7268dc6d94b4c0d34e6fbcbca5797a6b85c4066f
Jorge Moreno <jjmorenoaudio@gmail.com>
2024-12-16 20:29:21 +0000

    finnished

--- src/models.py
+++ src/models.py
@@ -14,13 +14,14 @@ class User(Base):
     username = Column(String(250), nullable=False)
     firstname = Column(String(250))
     lastname = Column(String(250))
-    email = Column(String(250))
+    email = Column(String(250), primary_key=True)
 
 class Comment(Base):
     __tablename__ = 'comment'
 
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(1000))
+    likes = Column(Integer)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
     
@@ -29,10 +30,13 @@ class Post(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
+    post_text = Column(String(1000))
+    likes = Column(Integer)
 
 class Follower(Base):
     __tablename__ = 'follower'
-
+    
+    id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
 
@@ -40,7 +44,6 @@ class Media(Base):
     __tablename__ = 'media'
   
     id = Column(Integer, primary_key=True)
-    type = Column()
     url = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'))
    
